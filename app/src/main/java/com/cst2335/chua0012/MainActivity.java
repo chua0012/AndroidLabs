20b1863519e13e1860dbb2e9bb9aa2b2e5b8ee44
Kyle Zachary Chua <chua0012@algonquinlive.com>
2022-05-24 18:31:08 -0400

    lab2_step2

--- app/src/main/java/com/cst2335/chua0012/MainActivity.java
+++ app/src/main/java/com/cst2335/chua0012/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_constraint);
 
         Button btn = findViewById(R.id.button);
         Switch sw = (Switch) findViewById(R.id.switch1);
