d02cc239a22e9094f4b0657fda3e481554fb7675
Kyle Zachary Chua <chua0012@algonquinlive.com>
2022-06-02 15:59:07 -0400

    lab2_step2

--- app/src/main/java/com/cst2335/chua0012/MainActivity.java
+++ app/src/main/java/com/cst2335/chua0012/MainActivity.java
@@ -39,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
                             .setAction("undo", new View.OnClickListener() {
                                 @Override
                                 public void onClick(View v) {
-
+                                    sw.toggle();
                                 }
                             });
                 }
